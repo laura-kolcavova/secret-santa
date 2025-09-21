3c044047a5d9878dc2af9dc28143cd5879630a7d
Laura Kolčavová <53749720+laura-kolcavova@users.noreply.github.com>
2025-09-21 19:41:55 +0200

    feat: add not found page (#6)

---
+++ client/src/pages/NotFound/index.tsx
@@ -0,0 +1,15 @@
+import { Component } from 'solid-js';
+import { FormattedMessage } from '~/translation/FormattedMessage';
+import { messages } from './messages';
+
+export const NotFound: Component = () => {
+  return (
+    <div class="container mx-auto py-12">
+      <div class="py-24 text-center">
+        <span class="text-xl font-bold text-pallete-4">
+          <FormattedMessage message={messages.pageNotFound} />
+        </span>
+      </div>
+    </div>
+  );
+};
