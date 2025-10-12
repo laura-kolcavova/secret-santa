1c1587453e0b273e6891464fa5381eb59693d642
Laura Kolčavová <53749720+laura-kolcavova@users.noreply.github.com>
2025-10-12 20:50:44 +0200

    feat: mutex (#45)

--- server/src/application/drawGroups/services/drawParticipantService.ts
+++ server/src/application/drawGroups/services/drawParticipantService.ts
@@ -3,42 +3,51 @@ import { DrawnParticipant } from '../models/DrawnParticipant';
 import { drawGroupErrors } from '../drawGroupErrors';
 import { drawGroupManager } from './drawGroupManager';
 import { drawRandomParticipant, findParticipantByEmail } from '../models/DrawGroup';
+import { Mutex } from 'async-mutex';
 
-const drawParticipant = (
+const mutex = new Mutex();
+
+const drawParticipant = async (
   drawGroupGuid: string,
   participantEmail: string,
   abortSignal: AbortSignal,
-): Result<DrawnParticipant> => {
-  const drawGroup = drawGroupManager.findByGuid(drawGroupGuid, abortSignal);
+): Promise<Result<DrawnParticipant>> => {
+  const releaseMutex = await mutex.acquire();
 
-  if (!drawGroup) {
-    return resultError(drawGroupErrors.notFound());
+  try {
+    const drawGroup = drawGroupManager.findByGuid(drawGroupGuid, abortSignal);
+
+    if (!drawGroup) {
+      return resultError(drawGroupErrors.notFound());
+    }
+
+    const participant = findParticipantByEmail(drawGroup, participantEmail);
+
+    if (!participant) {
+      return resultError(drawGroupErrors.userNotJoined());
+    }
+
+    if (participant.hasDrawn) {
+      return resultError(drawGroupErrors.userAlreadyDrawn());
+    }
+
+    const drawnParticipant = drawRandomParticipant(drawGroup, participant);
+
+    if (!drawnParticipant) {
+      return resultError(drawGroupErrors.noParticipantsToDraw());
+    }
+
+    const newDrawnParticipant = drawGroupManager.confirmDrawnParticipant(
+      drawGroup,
+      participant,
+      drawnParticipant,
+      abortSignal,
+    );
+
+    return resultSuccess(newDrawnParticipant);
+  } finally {
+    releaseMutex();
   }
-
-  const participant = findParticipantByEmail(drawGroup, participantEmail);
-
-  if (!participant) {
-    return resultError(drawGroupErrors.userNotJoined());
-  }
-
-  if (participant.hasDrawn) {
-    return resultError(drawGroupErrors.userAlreadyDrawn());
-  }
-
-  const drawnParticipant = drawRandomParticipant(drawGroup, participant);
-
-  if (!drawnParticipant) {
-    return resultError(drawGroupErrors.noParticipantsToDraw());
-  }
-
-  const newDrawnParticipant = drawGroupManager.confirmDrawnParticipant(
-    drawGroup,
-    participant,
-    drawnParticipant,
-    abortSignal,
-  );
-
-  return resultSuccess(newDrawnParticipant);
 };
 
 export const drawParticipantService = {
