9d989ac924d1c7ece1cd9c67fecdf8c8ec2bf141
Laura Kolčavová <laura.kolcavova@notino.com>
2025-11-12 22:14:35 +0100

    feat: update

--- server/src/api/drawGroups/editDrawGroup/editDrawGroupValidation.ts
+++ server/src/api/drawGroups/editDrawGroup/editDrawGroupValidation.ts
@@ -3,7 +3,7 @@ import { validationHandler } from '~/api/shared/middlewares/validationHandler';
 
 export const editDrawGroupValidation = [
   body('drawGroupGuid').notEmpty(),
-  body('name').notEmpty().isString(),
+  body('name').notEmpty().isString().escape().isLength({ max: 256 }),
   body('drawStartUtc').notEmpty().isISO8601(),
   body('drawEndUtc').notEmpty().isISO8601(),
   validationHandler,
