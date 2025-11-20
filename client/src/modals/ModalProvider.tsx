813478c4f12ecb251156e93acd9862519b75f78e
Laura Kolčavová <53749720+laura-kolcavova@users.noreply.github.com>
2025-11-20 22:52:32 +0100

    feat: modal handlers (#65)

--- client/src/modals/ModalProvider.tsx
+++ client/src/modals/ModalProvider.tsx
@@ -3,59 +3,105 @@ import {
   createContext,
   createRoot,
   createSignal,
+  For,
   JSX,
   ParentComponent,
   Show,
   useContext,
 } from 'solid-js';
+import { createStore, produce } from 'solid-js/store';
 
-export type ModalFactory = () => JSX.Element;
+type ModalFactory = (index: number) => JSX.Element;
+
+type ModalDisposer = () => void;
+
+type ModalHandler = {
+  create: ModalFactory;
+  dispose: ModalDisposer;
+};
 
 export type ModalContextValue = {
   openModal: (modalFactory: ModalFactory) => void;
   hideModal: () => void;
+  isCurrentModal: (index: number) => boolean;
 };
 
 const ModalContext = createContext<ModalContextValue | null>(null);
 
 export const ModalProvider: ParentComponent = (props) => {
-  const [getIsModalOpen, setIsModalOpen] = createSignal<boolean>(false);
-  const [getModal, setModal] = createSignal<ModalFactory | null>(null);
+  const [getModalCount, setModalCount] = createSignal<number>(0);
 
-  let disposeModal: (() => void) | null = null;
+  const [modalHandlers, setModalHandlers] = createStore<ModalHandler[]>([]);
 
-  const openModal = (modal: ModalFactory): void => {
-    if (disposeModal) {
-      disposeModal();
-    }
-
-    disposeModal = createRoot((dispose) => {
-      batch(() => {
-        setModal(() => modal);
-        setIsModalOpen(true);
-      });
-
-      return dispose;
+  const addModalHandler = (modalHandler: ModalHandler): void => {
+    batch(() => {
+      setModalCount((prev) => prev + 1);
+      setModalHandlers(produce((modalHandlers) => modalHandlers.push(modalHandler)));
     });
   };
 
-  const hideModal = (): void => {
-    if (disposeModal) {
-      disposeModal();
-      disposeModal = null;
+  const popModalHandler = (): void => {
+    if (modalHandlers.length === 0) {
+      return;
     }
 
     batch(() => {
-      setModal(null);
-      setIsModalOpen(false);
+      setModalCount((prev) => prev - 1);
+      setModalHandlers(produce((modalHandlers) => modalHandlers.pop()));
+    });
+  };
+
+  const getCurrentModalIndex = (): number => {
+    return modalHandlers.length - 1;
+  };
+
+  const getCurrentModalHandler = (): ModalHandler | undefined => {
+    const index = getCurrentModalIndex();
+
+    if (index === -1) {
+      return undefined;
+    }
+
+    return modalHandlers[index];
+  };
+
+  const isCurrentModal = (index: number): boolean => {
+    return index === getCurrentModalIndex();
+  };
+
+  const openModal = (modalFactory: ModalFactory): void => {
+    const modalDisposer = createRoot((dispose) => {
+      return dispose;
+    });
+
+    const modalHandler: ModalHandler = {
+      create: modalFactory,
+      dispose: modalDisposer,
+    };
+
+    addModalHandler(modalHandler);
+  };
+
+  const hideModal = (): void => {
+    if (modalHandlers.length === 0) {
+      return;
+    }
+
+    const currentHandler = getCurrentModalHandler()!;
+
+    batch(() => {
+      currentHandler.dispose();
+      popModalHandler();
     });
   };
 
   return (
-    <ModalContext.Provider value={{ openModal, hideModal }}>
+    <ModalContext.Provider value={{ openModal, hideModal, isCurrentModal }}>
       {props.children}
 
-      <Show when={getIsModalOpen()}>{getModal()?.()}</Show>
+      <Show when={getModalCount() > 0}>
+        <For each={modalHandlers}>{(modalHandler, index) => modalHandler.create(index())}</For>
+      </Show>
     </ModalContext.Provider>
   );
 };
